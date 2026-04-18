4600a30aae4b427b0f2d5107b6e6269084f67d0f
imthaivu <imquocthai@gmail.com>
2026-04-18 10:36:22 +0600

    init

---
+++ src/main/java/com/example/bookstoreuserservice/BookstoreUserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.bookstoreuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreUserServiceApplication.class, args);
+
+    }
+
+}
